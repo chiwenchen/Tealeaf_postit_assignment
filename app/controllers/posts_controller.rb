25d66e4222bd71a8d8f05b6174b789acc07d3d6b
Chiwen Chen <cwchen2000@gmail.com>
2015-04-04 18:29:01 +0800

    update validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-	require 'pry'
+	#require 'pry'
   
   def index
   	@posts = Post.all
@@ -11,19 +11,32 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(strong_params)
 
+    if @post.save
+      flash[:notice] = 'Your post is create successfully'
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
 
+  end
+
+  private
+
+  def strong_params
+    params.require(:post).permit! # permit! will permit all attributes
   end
 
 end
